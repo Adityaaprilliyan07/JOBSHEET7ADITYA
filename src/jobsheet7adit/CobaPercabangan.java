b365a59d37bd792cb1971db5de6f94272551a457
Asus <Asus@DESKTOP-R7PQKTL>
2018-08-01 18:01:32 +0700

    Coba IF..ELSE IF..ELSE.

--- src/jobsheet7adit/CobaPercabangan.java
+++ src/jobsheet7adit/CobaPercabangan.java
@@ -40,6 +40,27 @@ if (a<5) {
     System.out.println("nilai a kurang dari 5");
 } else System.out.println("nilai a lebih dari atau sama dengan 5 ");
 
+System.out.println("nilai kurang dari 5");
+if(a<5) {
+    System.out.println("nilai a kurang dari 5");
+} else if(a==5) {
+    System.out.println("nilai a sama dengan 5");
+
+System.out.println("\nIF..ELSE IF..ELSE.. 2");
+} else if(a<2) {
+    System.out.println("nilai a kurang dari 2");
+}if(a>4) {
+    System.out.println("nilai a kurang dari 4");
+} else if(a<6) {
+    System.out.println("nilai a kurang dari 6");
+} else if(a==6) {
+    System.out.println("nilai a sama dengan 6");
+} else {
+    System.out.println("nilai a lebih dari 6");
+    
+}
+    
+}
 }
 
 }
