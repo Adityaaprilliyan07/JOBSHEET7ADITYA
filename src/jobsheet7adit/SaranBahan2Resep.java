4f696e2f757806c136ce33d037b29ea1261ca264
Asus <Asus@DESKTOP-R7PQKTL>
2018-08-02 12:03:03 +0700

    Tambah Input Bahan 1

--- src/jobsheet7adit/SaranBahan2Resep.java
+++ src/jobsheet7adit/SaranBahan2Resep.java
@@ -5,6 +5,8 @@
  */
 package jobsheet7adit;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Asus
@@ -15,7 +17,13 @@ public class SaranBahan2Resep {
         System.out.println("Identitas : "+identitas);
         
         System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
-        
+         Scanner scanner = new Scanner(System.in);
+         
+         System.out.println("Bahan Pertama");
+         System.out.println("1. Pisang");
+         System.out.println("2. Telur");
+         System.out.println("Masukkan no pilihan anda :");
+         int bahan1 = scanner.nextInt();
                 
     }
     
