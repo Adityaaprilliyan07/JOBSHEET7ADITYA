138306ee54c5ab7dc8a37092480cfb021e132aa7
Asus <Asus@DESKTOP-R7PQKTL>
2018-08-03 07:14:09 +0700

    Initial Commit

---
+++ src/SaranBahan2Resep.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Asus
+ */
+public class SaranBahan2Resep {
+    public static void main(String[] args) {
+    String identitas = "nama lengkap / kelas / no";
+     System.out.println("Identitas : "+ identitas);
+     
+      System.out.print("\nSaran Resep dari Bahan Milik Anda\n");
+      
+    
+}
